2d4cff8da86d381288231262e6e4cbcab618235e
Valentina Gonzalez <valentina.gonzalez@atropay.com>
2019-08-20 19:07:59 -0300

    deleted Valentina name

--- Program.cs
+++ Program.cs
@@ -7,7 +7,6 @@ namespace Programacion_II
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("Valentina Gonzalez")
         }
     }
 }
